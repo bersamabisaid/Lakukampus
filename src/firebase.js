66cd5b35bb5f83d1076c88811b36cf6ca9caf954
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2020-10-23 19:33:30 +0700

    feat: :wrench: resetting firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import * as firebase from 'firebase/app';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-    apiKey: 'AIzaSyCh4TVIDno6AkM0NDO9XOi02PaKKx5xtxc',
-    authDomain: 'lakukampus-dev.firebaseapp.com',
-    databaseURL: 'https://lakukampus-dev.firebaseio.com',
-    projectId: 'lakukampus-dev',
-    storageBucket: 'lakukampus-dev.appspot.com',
-    messagingSenderId: '452090096078',
-    appId: '1:452090096078:web:da34efa8d4802cb344e39f',
-    measurementId: 'G-V1QXB3MRD8',
+    apiKey: 'AIzaSyAgDk-stNuxiPvzU2C9Kk4rqIrqqPq0RaI',
+    authDomain: 'lakukampus-e-commerce.firebaseapp.com',
+    databaseURL: 'https://lakukampus-e-commerce.firebaseio.com',
+    projectId: 'lakukampus-e-commerce',
+    storageBucket: 'lakukampus-e-commerce.appspot.com',
+    messagingSenderId: '233584393898',
+    appId: '1:233584393898:web:0d0bf49af56506bba91ad3',
+    measurementId: 'G-NNTVHN8D04',
 };
 
 firebase.initializeApp(firebaseConfig);
