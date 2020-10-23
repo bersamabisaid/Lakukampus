7c48678faf2b91f4f13a700300b438527934be10
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2020-10-23 18:37:13 +0700

    feat: :lipstick: add coming soon watermark

--- src/App.vue
+++ src/App.vue
@@ -8,3 +8,16 @@ export default {
     name: 'App',
 };
 </script>
+
+<style lang="scss">
+#q-app::after {
+    content: 'COMING SOON';
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    text-align: center;
+    font-size: map-get($h1, size);
+    text-shadow: 0 0 2rem #fffc;
+    transform: translate(-50%, -50%);
+}
+</style>
