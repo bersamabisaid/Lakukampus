f7610e10af68a44ff01366b28bd6abd3440e05c7
Abyanzhafran <52287739+Abyanzhafran@users.noreply.github.com>
2021-01-17 16:58:56 +0700

    Try commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -79,6 +79,7 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
     'prefer-promise-reject-errors': 'off',
+    'linebreak-style': 0,
 
     // TypeScript
     quotes: ['warn', 'single', { avoidEscape: true }],
