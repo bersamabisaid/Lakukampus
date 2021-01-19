df0e0e2c56fc1f95d29f0b4df254b13fed1c0806
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2021-01-19 17:05:12 +0700

    perf: :zap: change authGuard watch dependency

--- src/composition/useAuthGuard.ts
+++ src/composition/useAuthGuard.ts
@@ -4,16 +4,20 @@ import { Loading } from 'quasar';
 
 export default (props: unknown, ctx: SetupContext) => {
   watch(
-    authState,
-    async () => {
-      if (authState.isWaitingAuthentication) {
+    [() => authState.isWaitingAuthentication, signedInUser],
+    async (val) => {
+      const [
+        waitAuthState, user,
+      ] = val as [typeof authState.isWaitingAuthentication, typeof signedInUser.value];
+
+      if (waitAuthState) {
         Loading.show({
           message: 'authenticating...',
         });
       } else {
         Loading.hide();
 
-        if (!signedInUser.value) {
+        if (!user) {
           await ctx.root.$router.push({ name: 'Auth' });
         }
       }
