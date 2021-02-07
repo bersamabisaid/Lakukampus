9d427a15ac282685377facdc92ac80864eb8fa16
Abyanzhafran <52287739+Abyanzhafran@users.noreply.github.com>
2021-02-07 22:44:42 +0700

    fix(page): :bug: fix carousel bug

--- src/components/ui/ImageDetailCarousel.vue
+++ src/components/ui/ImageDetailCarousel.vue
@@ -1,38 +1,42 @@
 <template>
   <div class="cursor-pointer q-gutter-sm">
     <q-carousel
-      swipeable
       animated
       v-model="slide"
-      infinite
-      thumbnails
-      @click="dialogClick"
+      navigation
       arrows
-      class="scroll oveflow-hidden"
+      swipeable
+      infinite
     >
       <q-carousel-slide
-        name="1"
+        @click="dialogClick"
+        :name="1"
         img-src="https://cdn.quasar.dev/img/mountains.jpg"
       />
       <q-carousel-slide
-        name="2"
+        @click="dialogClick"
+        :name="2"
         img-src="https://cdn.quasar.dev/img/parallax1.jpg"
       />
       <q-carousel-slide
-        name="3"
+        @click="dialogClick"
+        :name="3"
         img-src="https://cdn.quasar.dev/img/parallax2.jpg"
       />
       <q-carousel-slide
-        name="4"
+        @click="dialogClick"
+        :name="4"
         img-src="https://cdn.quasar.dev/img/quasar.jpg"
       />
       <q-carousel-slide
-        name="5"
-        img-src="https://cdn.quasar.dev/img/quasar.jpg"
-      />
-      <q-carousel-slide
-        name="6"
+        @click="dialogClick"
+        :name="5"
         img-src="https://cdn.quasar.dev/img/parallax2.jpg"
+      />
+      <q-carousel-slide
+        @click="dialogClick"
+        :name="6"
+        img-src="https://cdn.quasar.dev/img/mountains.jpg"
       />
     </q-carousel>
     <detail-card-dialog v-model="dialog" />
@@ -46,7 +50,7 @@ export default {
   name: 'ImageDetailCarousel',
 
   data: () => ({
-    slide: '1',
+    slide: 1,
     fullscreen: false,
     dialog: false,
   }),
